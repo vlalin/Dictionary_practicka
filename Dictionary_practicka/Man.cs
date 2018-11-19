9032f479b40adef2789c13523c45af4ac2978333
student <student@101-12>
2018-11-19 10:29:00 +0200

    Add project files.

---
+++ Dictionary_practicka/Man.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictionary_practicka
+{
+    class Man
+    {
+        public string name { get; set; }
+        public string surname { get; set; }
+        public string patronymic { get; set; }
+
+        public int age { get; set; }
+    }
+}
---
+++ Dictionary_practicka/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary_practicka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, Man> keyValuePairs = new Dictionary<int, Man>();
+
+            Add(keyValuePairs);
+            items(keyValuePairs);
+
+            ManSearch(keyValuePairs, Human_inventory());
+
+            Console.Read();
+        }
+
+        static Man Human_inventory()
+        {
+            Man man = new Man();
+
+            Console.Write("Введите имя: ");
+            string name = Console.ReadLine();
+            Console.Write("Введите фамилию: ");
+            string surname = Console.ReadLine();
+            Console.Write("Введите отчество: ");
+            string patronymic = Console.ReadLine();
+
+            man.name = name;
+            man.surname = surname;
+            man.patronymic = patronymic;
+
+            int age = 0;
+
+            Console.Write("Введите возрвст: ");
+            Int32.TryParse(Console.ReadLine(), out age);
+
+            man.age = age;
+
+            return man;
+        }
+
+        static void Add(Dictionary<int, Man> keyValuePairs)
+        {
+            keyValuePairs.Add(0, Human_inventory());       
+        }
+
+        static void ManSearch(Dictionary<int, Man> dict, Man key)
+        {
+            foreach (var item in dict)
+            {
+                if(item.Value == key)
+                    Console.WriteLine("Найден!");
+            }
+        }
+
+        static void items(Dictionary<int, Man> keyValuePairs)
+        {
+            foreach (var item in keyValuePairs)
+            {
+                Console.Write(item.Key);
+                Console.Write(item.Value.name);
+                Console.Write(item.Value.surname);
+                Console.Write(item.Value.patronymic);
+                Console.Write(item.Value.age);
+            }
+            
+        }
+    }
+}
